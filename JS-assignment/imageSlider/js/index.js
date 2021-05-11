b698633dc0fe73d192f413bc2fcb1768d1e2902a
Irajan <38995844+Irajan@users.noreply.github.com>
2021-05-11 11:12:21 +0545

    remove comments

--- JS-assignment/imageSlider/js/index.js
+++ JS-assignment/imageSlider/js/index.js
@@ -5,7 +5,7 @@ const slowerSlider = $('#slower-slider');
 
 window.onload = function () {
 	new ImageSlider(fasterSlider, 40, 1000);
-// 	new ImageSlider(fastSlider, 100, 2000);
-// 	new ImageSlider(slowSlider);
-// 	new ImageSlider(slowerSlider, 700, 4000);
+ 	new ImageSlider(fastSlider, 100, 2000);
+ 	new ImageSlider(slowSlider);
+ 	new ImageSlider(slowerSlider, 700, 4000);
 };
